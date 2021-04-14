34292217afb45e4be4370772c3dae07e3daab920
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-14 14:30:31 +0200

    Added functionality for shuffling and displaying cards

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 
 import './Main.css'
 
 import getImages from './Helpers/getImages'
 
+function Board(props) {
+
+    return(
+        props.cards.map(card => {
+            console.log(card)
+            return (
+                <div className='card' key={card.id}>
+                    <img src={card.url}></img>
+                </div>
+            )
+        })
+    )
+
+}
 
 function Main(props) {
-    const images = getImages()
-    console.log(images)
+
+    const [images, setImages] = useState(getImages())
+
+    function shuffleArray(array) {
+        let shuffledArray = [...array]
+        console.log(shuffledArray)
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            var temp = shuffledArray[i];
+            shuffledArray[i] = shuffledArray[j];
+            shuffledArray[j] = temp;
+        }
+        return shuffledArray
+    }
 
     return (
-        <main>
-            <div className='board'>
+        <main className='flex justify-center'>
+            <div className='flex gameboard'>
+                <Board cards={shuffleArray(images)}/>
             </div>
+            
         </main>
     )
 }

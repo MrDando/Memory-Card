7ef1c037d667c8629e404b33b7cfa5d71c95d1b3
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-16 10:51:27 +0200

    Added card flipping animation

--- src/Components/Main/Components/Board.js
+++ src/Components/Main/Components/Board.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Board(props) {
+
+    let [cardClass, setCardClass] = useState('flip-card-inner is-flipped')
+
+    useEffect(() => {
+        setTimeout(function(){setCardClass('flip-card-inner')}, 500)
+    },[props])
+
+    function flipCard(e) {
+        setCardClass('flip-card-inner is-flipped')
+
+        setTimeout(function(){props.selectCard(e)}, 500)
+
+    }
 
     return(
         <div className='flex gameboard'>
             {props.cards.map(card => {
             return (
-                <div className='card' key={card.id}>
-                    <img src={card.url} alt='' data-id={card.id} onClick={props.selectCard}></img>
+                <div className='flip-card' key={card.id}>
+                    <div className={cardClass}>
+                        <div className='flip-card-front'>
+                            <img src={card.url} alt='' data-id={card.id} onClick={flipCard}></img>
+                        </div>
+                        <div className='flip-card-back flex column justify-center'>
+                            <h3>Memory</h3>
+                        </div>
+                    </div>
                 </div>
             )
             })}

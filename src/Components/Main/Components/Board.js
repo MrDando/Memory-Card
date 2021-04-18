fb54baf4eff0e60637ab60b22c889212394579a5
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-18 23:19:34 +0200

    Slowed down card flipping animation

--- src/Components/Main/Components/Board.js
+++ src/Components/Main/Components/Board.js
@@ -5,31 +5,31 @@ function Board(props) {
     let [cardClass, setCardClass] = useState('flip-card-inner is-flipped')
 
     useEffect(() => {
-        setTimeout(function(){setCardClass('flip-card-inner')}, 500)
+        setTimeout(function(){setCardClass('flip-card-inner')}, 1000)
     },[props])
 
     function flipCard(e) {
         setCardClass('flip-card-inner is-flipped')
 
-        setTimeout(function(){props.selectCard(e)}, 500)
+        setTimeout(function(){props.selectCard(e)}, 1000)
 
     }
 
     return(
         <div className='flex gameboard'>
             {props.cards.map(card => {
-            return (
-                <div className='flip-card' key={card.id}>
-                    <div className={cardClass}>
-                        <div className='flip-card-front'>
-                            <img src={card.url} alt='' data-id={card.id} onClick={flipCard}></img>
-                        </div>
-                        <div className='flip-card-back flex column justify-center'>
-                            <h3>Memory</h3>
+                return (
+                    <div className='flip-card' key={card.id}>
+                        <div className={cardClass}>
+                            <div className='flip-card-front'>
+                                <img src={card.url} alt='' data-id={card.id} onClick={flipCard}></img>
+                            </div>
+                            <div className='flip-card-back flex column justify-center'>
+                                <h3>Memory</h3>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )
+                )
             })}
         </div>
     )
